c41762033ad42056230bdb4d933a411fcb3ccd0d
Folgui <aguante.los.rulos@gmail.com>
2017-09-29 10:05:07 -0300

    MegaMan, Movimientos hechos, disparos casi hechos

---
+++ MegaMan_Expo/Clases/Base.py
@@ -0,0 +1,24 @@
+import pygame
+from .Grupos import *
+
+
+class Base(pygame.sprite.Sprite):
+
+    sprites = Sprites()
+    Balas = Sprites()
+    piso = Sprites()
+    bloques = Sprites()
+
+    def __init__(self, x, y, ancho, alto, ruta):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(ruta)
+        self.image = pygame.transform.scale(self.image, (ancho, alto))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.ancho = ancho
+        self.alto = alto
---
+++ MegaMan_Expo/Main.py
@@ -0,0 +1,27 @@
+import sys
+from Clases import *
+
+Principal = Mega_Man(20, 375, 90,90,"Imagenes/Inclinado.png")
+Fondo = Fondo()
+ancho = 1280
+alto = 720
+ventana = Controlador.configurar_pantalla(ancho, alto)
+reloj = Controlador.iniciar_reloj()
+colores = {"Blanco": (255,255,255), "Negro": (0,0,0)}
+FPS = 20
+frames_totales = 0
+segundo = 0
+
+Colocacion()
+
+while True:
+
+    Acciones(reloj, Principal, FPS, frames_totales)
+
+    if frames_totales % (FPS / 4) == 0:
+        segundo += 0.25
+
+    dibujo(Fondo, ventana, colores)
+
+
+    frames_totales += 1
